c7dd461a91504b0ff504283f2c11b46bb1c411c6
unknown <heykassio@live.com>
2020-06-06 12:36:26 -0300

    Finalizando a aula 5 da nlw

---
+++ src/database/db.js
@@ -0,0 +1,75 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database("./src/database/database.db");
+
+
+module.exports = db;
+
+/*db.serialize(()=>{
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,            
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `);
+
+    const query = `
+        INSERT INTO places(
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);          
+    `;
+
+    const values = [
+        "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "Colectoria",
+        "Guilherme Gembella, Jardim América",
+        "Número 260",
+        "Santa Catarina",
+        "Rio do Sul",
+        "Resíduos eletrônicos, Lâmpadas"
+    ];
+
+    function afterInsert (error){
+        if(error){
+            return console.log(error);
+        };
+        console.log(`Cadastrado com sucesso!`);
+        console.log(this);
+    }
+
+    db.run(query, values, afterInsert);
+
+    db.all(`SELECT * FROM places`, function(error, rows){
+        if(error){
+            return console.log(error);
+        };
+        console.log('Aqui estão os registros:');
+        console.log(rows);
+    });
+
+    db.run(`DELETE FROM places WHERE id= ?`, [2], function(error){
+        if(error){
+            return console.log(error);
+        };
+        console.log(`Registro deletado com sucesso!`);
+    });    
+
+});*/
+// db.run(`DELETE FROM places WHERE id= ?`, [6], function(error){
+//     if(error){
+//         return console.log(error);
+//     };
+//     console.log(`Registro deletado com sucesso!`);
+// });  
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const server = express();
 const nunjucks = require('nunjucks');
+const db = require('./database/db');
 
 const port = normalizePort(process.env.PORT || '4000');
 
 server.use(express.static('public'));
+server.use(express.urlencoded({extended: true}));
 
 nunjucks.configure('src/views', {
     express: server,
@@ -20,8 +22,47 @@ server.get('/create-point', (req, res)=>{
     return res.render(`create-point.html`);
 });
 
-server.get('/search', (req, res)=>{
-    return res.render(`search-results.html`);
+server.post('/savepoint', (req, res)=>{
+    console.log(req.body);
+
+    const {image, name, address, address2, state, city, items} = req.body;
+    const query = `
+        INSERT INTO places(
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES(?,?,?,?,?,?,?);
+    `;
+
+    const values = [image, name, address, address2, state, city, items];
+
+    function afterInsert(error){
+        if(error){
+            return console.log(error);
+        };
+        console.log('Cadastrado com sucesso!');
+        console.log(this);
+        return res.render(`create-point.html`, {saved: true});
+    };
+
+    db.run(query, values, afterInsert);
+    
+});
+
+server.post('/search', (req, res)=>{
+    const {search} = req.body;
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(error, rows){
+        if(error){
+            return console.log(error);
+        }
+        return res.render(`search-results.html`, {places:  rows});
+    });
+        
 });
 
 server.listen(port);
